2e269d0f08764a34018f30280e292f31e5c6f1a1
Rasmus Noerskov <rasmus@norskov.org>
2019-05-23 23:54:38 +0200

    include build

---
+++ es/utils/load-image-url.js
@@ -0,0 +1,23 @@
+/* eslint-env browser, node */
+
+function isDataURL(str) {
+  if (str === null) {
+    return false;
+  }
+  var regex = /^\s*data:([a-z]+\/[a-z]+(;[a-z-]+=[a-z-]+)?)?(;base64)?,[a-z0-9!$&',()*+;=\-._~:@/?%\s]*\s*$/i;
+  return !!str.match(regex);
+}
+
+export default function loadImageURL(imageURL, crossOrigin) {
+  return new Promise(function (resolve, reject) {
+    var image = new Image();
+    image.onload = function () {
+      return resolve(image);
+    };
+    image.onerror = reject;
+    if (isDataURL(imageURL) === false && crossOrigin) {
+      image.crossOrigin = crossOrigin;
+    }
+    image.src = imageURL;
+  });
+}
